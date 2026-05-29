4608578f507cddfe0b0857217d359a8fc3718d08
isiah-yllan <99546608+isiah-yllan@users.noreply.github.com>
2026-05-29 12:57:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,56 @@
-print("Army Board Study Bot")
-print("--------------------")
+print('=== Army Board Study Bot ===\n')
+
 
 score = 0
 
-# Question 1 
-answer = input("1. What does NCO stand for? ")
-answer = answer.strip().lower()
 
-if answer == "noncommissioned officer":
-    print("Correct!\n")
-    score = score + 1
+questions = {
+    "What does NCO stand for?": "noncommissioned officer",
+    "What regulation covers Army wear and appearance?": "ar 670-1",
+    "How many Army Values are there?": "7",
+    "What does PT stand for?": "physical training",
+    "What rank is E-4?": "specialist",
+    "What does AIT stand for?": "advanced individual training",
+    "What does PCS stand for?": "permanent change of station",
+    "What does CONUS stand for?": "continental united states",
+    "What does OCONUS stand for": "outside continental united states",
+    "What does MRE stand for?": "meal ready to eat", 
+    "What Army regulation covers Army Leadership?": "adp 6-22",
+    "How many Army Values begin with the letter L?" : "1",
+    "What does ASAP stand for?": "army substance abuse program",
+    "What rank is E-5?": "sergeant",
+    "What rank is E-6?": "staff sergeant",
+    "What rank is O-1?": "second lieutenant",
+    "What does UCMJ stand for?": "uniform code of military justice",
+    "What is the Army motto?": "this we'll defend",
+    "What does POV stand for?": "privatley owned vehicle",
+    "What does MOS stand for?": "military occupational specialty"
+    }
+
+for question, correct_answer in questions.items():
+
+
+    answer = input(question+" ")
+    answer = answer.strip().lower()
+
+    if answer== correct_answer:
+        print("Correcto!\n")
+        score +=1
+       
+    else:
+        print("Incorrect.")
+        print("Correct Answer:", correct_answer)
+        print()
+
+
+print("======================")
+print("Final Score:", score, "/20")
+
+if score >= 18:
+    print("🏆 BOARD READY")
+elif score >= 15:
+    print("✅ Almost Board Ready")
+elif score >= 10:
+    print ("📚 Keep Studying")
 else:
-    print("Incorrect. NCO stands for Noncommissioned Officer.\n")
-
-# Question 2
-answer = input("2. what regulation covers Army uniforms and appearance?")
-answer = answer.strip().lower()
-
-if answer == "ar 670-1":
-    print("Correct!\n")
-    score += 1
-else:
-    print("Incorrect. The answer is AR 670-1.\n")
-
-# Question 3
-answer = input("3. How many Army values are there?")
-answer = answer.strip()
-
-if answer == "7":
-    print("Correct!\n")
-    score += 1
-else:
-    print("Incorrect. There are 7 Army values.\n")
-
-# Results
-print("==================")
-print("Final Score:", score, "/3")
-
-if score == 3:
-    print("Outstanding! Ready for the board.")
-elif score == 2:
-    print("Good Job. Study a little more.")
-elif score == 1:
-    print("Keep Studying.")
-else:
-    print("Hit the books, Specialist.")
+    print("🚨Hit the Books, Specialist")
